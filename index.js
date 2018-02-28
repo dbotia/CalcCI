4992a5fc266e5db2449427cc7c8b5f4bba8f0bf8
Unknown <diego.botia@gmail.com>
2018-02-28 11:35:18 -0500

    cambio

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var calculator = require('./app/calculator');
 
 var a = 3;
 var b = 5;
-
+//prueba de salida
 console.log(`Add ${a} + ${b} = ${calculator.add(a,b)}`);
 console.log(`Minus ${a} + ${b} = ${calculator.minus(a,b)}`);
 console.log(`Multiply ${a} + ${b} = ${calculator.multiply(a,b)}`);
